08fbf8454737c245633836ae6ded2decfb701d72
leehitchcock <lee.hitchcock2041@gmail.com>
2020-07-18 22:04:07 +0100

    Create POST user saga

--- src/services/sagas/userAuthSaga.js
+++ src/services/sagas/userAuthSaga.js
@@ -1,5 +1,29 @@
-import { fork } from 'redux-saga/effects';
+import { fork, takeLatest, put } from 'redux-saga/effects';
+import { REGISTER_USER } from '../actions';
+import { registerUser } from '../actions/userAuthActions';
 
-function* userAuthSaga() {}
+function* registerUserSaga() {
+	try {
+		const targetURL = '/api/auth/';
+		const data = {
+			emailAddress: 'lee.hitchcock2041@gmail.com',
+			password: 'letmein1234!'
+		};
+		const response = yield fetch(targetURL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		});
+		yield put(registerUser.success(response.data));
+	} catch (e) {
+		yield put(registerUser.fail(e));
+	}
+}
 
-export default [fork(userAuthSaga)];
+function* watchUserRegistration() {
+	yield takeLatest(REGISTER_USER.REQUEST, registerUserSaga);
+}
+
+export default [fork(watchUserRegistration)];
