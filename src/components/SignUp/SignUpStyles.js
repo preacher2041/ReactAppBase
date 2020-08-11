015a7ced11c5bb38e44d7c0ddf316b73815be920
leehitchcock <lee.hitchcock2041@gmail.com>
2020-08-11 20:39:51 +0100

    Create Sign Up Modal

--- src/components/SignUp/SignUpStyles.js
+++ src/components/SignUp/SignUpStyles.js
@@ -1,15 +1,70 @@
-export const styles = theme => ({
-	root: {
-		flexGrow: 1,
+export const styles = (theme) => ({
+	dialogTitle: {
+		width: '464px',
+		padding: '24px 24px 24px 24px'
 	},
-	grow: {
-		flexGrow: 1,
-		textDecoration: 'none'
+	closeButton: {
+		position: 'absolute',
+		left: theme.spacing(1),
+		top: theme.spacing(1),
+		color: theme.palette.grey[500]
 	},
-	appBar: {
-		background: '#F7F7F7',
+	dialogText: {
+		textAlign: 'center'
 	},
-	title: {
-		color: '#0C0C0C',
+	dialogTitleText: {
+		fontWeight: 700,
+		marginTop: '-7px',
+		marginBottom: '12px'
 	},
+	dialogSubTitleText: {
+		fontWeight: 300,
+		marginBottom: '-2px'
+	},
+	dialogContent: {
+		width: '464px',
+		padding: '0 24px 24px 24px'
+	},
+	textField: {
+		width: '100%',
+		height: '48px',
+		marginBottom: '24px'
+	},
+	textFieldInput: {
+		'& input': {
+			paddingTop: '15px',
+			paddingBottom: '15px'
+		}
+	},
+	textFieldIcon: {
+		color: '#D8D8DA'
+	},
+	button: {
+		width: '100%',
+		height: '48px',
+		boxShadow: 'none',
+		'&:hover': {
+			boxShadow: 'none'
+		},
+		marginBottom: '10px'
+	},
+	checkbox: {
+		marginBottom: '-11px'
+	},
+	dialogFooter: {
+		width: '464px',
+		padding: '24px 24px 24px 24px',
+		borderTop: '1px solid #D8D8DA'
+	},
+	dialogFooterText: {
+		textAlign: 'center',
+		fontWeight: 300
+	},
+	dialogFooterLinks: {
+		fontWeight: 400
+	},
+	signInLink: {
+		marginTop: '-5px',
+		marginBottom: '-2px'
+	}
 });
--- src/components/SignUp/SignUpView.jsx
+++ src/components/SignUp/SignUpView.jsx
@@ -1,16 +1,135 @@
 import React from 'react';
+import {
+	Button,
+	Checkbox,
+	DialogContent,
+	FormGroup,
+	FormControlLabel,
+	IconButton,
+	InputAdornment,
+	Link,
+	TextField,
+	Typography
+} from '@material-ui/core';
+import {
+	Close,
+	EmailRounded,
+	Visibility,
+	VisibilityOff
+} from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
 
 import { styles } from './SignUpStyles';
 
-export const SignUpModalTitle = withStyles(styles)(() => {
-	return <>Sign Up Titile</>;
+export const SignUpModalTitle = withStyles(styles)((props) => {
+	const { classes, onClose } = props;
+	return (
+		<div className={classes.dialogTitle}>
+			{onClose ? (
+				<IconButton className={classes.closeButton} onClick={onClose}>
+					<Close />
+				</IconButton>
+			) : null}
+
+			<Typography
+				className={`${classes.dialogText} ${classes.dialogTitleText}`}
+				variant="h2">
+				Hello!
+			</Typography>
+			<Typography
+				className={`${classes.dialogText} ${classes.dialogSubTitleText}`}
+				variant="body1">
+				Sign up for an account here
+			</Typography>
+		</div>
+	);
 });
 
-export const SignUpModalContent = withStyles(styles)(() => {
-	return <>Sign Up Content</>;
+export const SignUpModalContent = withStyles(styles)((props) => {
+	const visibility = false;
+	const { classes } = props;
+	const emailInputProps = {
+		className: `${classes.textFieldInput}`,
+		startAdornment: (
+			<InputAdornment position="start">
+				<EmailRounded color="disabled" />
+			</InputAdornment>
+		)
+	};
+	const passwordInputProps = {
+		className: `${classes.textFieldInput}`,
+		startAdornment: (
+			<InputAdornment position="start">
+				{visibility ? (
+					<Visibility color="disabled" />
+				) : (
+					<VisibilityOff color="disabled" />
+				)}
+			</InputAdornment>
+		)
+	};
+
+	const termsAndConditionsText = (
+		<Typography>
+			I agree to the &nbsp;
+			<Link>Terms & Conditions</Link>
+		</Typography>
+	);
+
+	const newsLetterText = (
+		<Typography>I want to subscribe to the newsletter</Typography>
+	);
+
+	const checkbox = <Checkbox color="primary" />;
+
+	return (
+		<DialogContent className={classes.dialogContent}>
+			<TextField
+				className={classes.textField}
+				placeholder="Email"
+				variant="outlined"
+				InputProps={emailInputProps}
+			/>
+			<TextField
+				className={classes.textField}
+				placeholder="Password"
+				variant="outlined"
+				InputProps={passwordInputProps}
+			/>
+			<Button
+				className={classes.button}
+				variant="contained"
+				color="primary">
+				Sign In
+			</Button>
+			<FormGroup aria-label="position" row>
+				<FormControlLabel
+					value="termsAndConditions"
+					control={checkbox}
+					label={termsAndConditionsText}
+					labelPlacement="right"
+				/>
+				<FormControlLabel
+					className={classes.checkbox}
+					value="newsletter"
+					control={checkbox}
+					label={newsLetterText}
+					labelPlacement="right"
+				/>
+			</FormGroup>
+		</DialogContent>
+	);
 });
 
-export const SignUpModalActions = withStyles(styles)(() => {
-	return <>Sign Up Actions</>;
+export const SignUpModalActions = withStyles(styles)((props) => {
+	const { classes } = props;
+	return (
+		<div className={classes.dialogFooter}>
+			<Typography
+				className={`${classes.dialogFooterText} ${classes.signInLink}`}>
+				Already have an account?&nbsp;
+				<Link className={classes.dialogFooterLinks}>Sign In</Link>
+			</Typography>
+		</div>
+	);
 });
